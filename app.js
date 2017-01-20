dd55d358389c0e667375f25a894c49e8395fd187
Saroj Shrestha <Shrestha@Sarojs-MacBook-Pro.local>
2017-01-20 21:24:22 +0900

    Or | was wrong on app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const hbs = require('hbs');
 const app=express();
 const fs = require('fs');
 
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 
 app.set('view engine','hbs');
 hbs.registerPartials(__dirname + '/views/partial')
